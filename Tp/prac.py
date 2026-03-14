dc3a75fe9d660e995ee9aecef5830b3ec067aed0
Codey <nishchal@Nishchals-MacBook-Air.local>
2026-03-14 23:11:11 +0530

    Added some sorting algorithms

--- Tp/prac.py
+++ Tp/prac.py
@@ -1,1 +1,10 @@
-print(int("1"*99989) - pow(2,21768))
+def bubble_sort(l):
+    for i in range(len(l) - 1):
+        if l[i] > l[i+1]:
+            continue
+        else :
+            a = l[i]
+            l[i] = l[i+1]
+            l[i+1] = a
+    return l
+print(bubble_sort([1,2,3,4]))

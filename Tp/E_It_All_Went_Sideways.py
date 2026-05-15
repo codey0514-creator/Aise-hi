7ff385a6ddf62a3fc806161cdc31e701792b7457
Nishchal Jain <codey0514@gmail.com>
2026-05-15 23:52:45 +0530

    Updated files and added new documents

--- Tp/E_It_All_Went_Sideways.py
+++ Tp/E_It_All_Went_Sideways.py
@@ -1,22 +1,28 @@
 def solve():
     n = int(input())
-    l = list(map(int, input().split()))
-    def shift(l1):
-        bloc = 0
-        for i in range(len(l1)-2 , -1 , -1 ):
-            if l1[i] > l1[i+1]:
-                d = l1[i] - l1[i+1]
-                l1[i+1] += d
-                l1[i] -= d
-                bloc += d
-        return bloc
+    arr = list(map(int, input().split()))
+    l1 = arr[:]
     bloc = 0
-
-for i in range(len(l) - 1, 0, -1):   # iterate from end to start
-    temp = l[:]                       # copy list
-    temp[i] -= 1                      # subtract 1 from current index
-
-    bloc = max(bloc, shift(temp))
-i = int(input())
-for _ in range(i):
+    for i in range(n-2, -1, -1):
+        if l1[i] > l1[i+1]:
+            d = l1[i] - l1[i+1]
+            l1[i+1] += d
+            l1[i] -= d
+            bloc += d
+    last_occ = [-1] * (n + 2)
+    for j in range(n):
+        last_occ[arr[j]] = j
+    last_below = [-1] * (n + 2)
+    for v in range(1, n + 2):
+        last_below[v] = max(last_below[v-1], last_occ[v-1])
+    ans = bloc
+    for i in range(n):
+        h = arr[i]
+        lb = last_below[h]
+        if lb < i:
+            bonus = i - 1 - lb
+            ans = max(ans, bloc + bonus)
+    print(ans)
+t = int(input())
+for _ in range(t):
     solve()
